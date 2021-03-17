c27b81bf94fe97efda3765c782f1c58447815003
Kolja Kauder <kkauder@gmail.com>
2021-03-17 12:41:40 -0400

    Cleanup and unification

--- slurm_p6_eicsmear_root/submit_to_slurm.sh
+++ slurm_p6_eicsmear_root/submit_to_slurm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-BASEDIR=`dirname $0`
-BASEDIR=`readlink -f ${BASEDIR}`
-OUTPUT=${BASEDIR}/output
-SCRIPT=${BASEDIR}/epCreateSimu.sh
+SOURCE_DIR=`dirname $0`
+SOURCE_DIR=`readlink -f ${BASEDIR}`
+OUTPUT=${SOURCE_DIR}/output
+SCRIPT=${SOURCE_DIR}/epCreateSimuSlurm.sh
 LIMIT=${1:-10}
 Q2LO=${2:-0.00001}
 Q2HI=${3:-1.0}
@@ -13,4 +13,4 @@ PNRG=${5:-100}
 echo "`basename ${SCRIPT}` for Q2 from ${Q2LO} to ${Q2HI}, Ee=${ENRG}, Ep=${PNRG} (${LIMIT} jobs)"
 
 sbatch --account=eic --partition=production --mem-per-cpu=2G --time=1-00:00:00 --constraint=farm18 --array=1-${LIMIT} \
-  ${SCRIPT} ${OUTPUT} ${Q2LO} ${Q2HI} ${ENRG} ${PNRG}
+  ${SCRIPT} ${Q2LO} ${Q2HI} ${ENRG} ${PNRG} ${OUTPUT} ${SOURCE_DIR}
