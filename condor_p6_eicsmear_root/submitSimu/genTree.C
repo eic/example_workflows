11b8df4064350df3e23043e6d49130630478795e
Kolja Kauder <kkauder@gmail.com>
2021-03-17 12:51:59 -0400

    Restructured; now using a common payload

--- condor_p6_eicsmear_root/submitSimu/genTree.C
+++
@@ -1,9 +0,0 @@
-#include <eicsmear/functions.h>
-
-void genTree(const std::string x, const std::string y) // TString
-{
-  gSystem->Load("libeicsmear");
-  gSystem->Load("libeicsmeardetectors");
-  BuildTree(x,y);
-}
-
