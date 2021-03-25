d595df8fa13b8a1b354a07c4353b50a35a4ad9c3
Kolja Kauder <kkauder@gmail.com>
2021-03-25 12:11:21 -0400

    pathfix

--- simu_smear_ana/panda_simu/env.sh
+++ simu_smear_ana/panda_simu/env.sh
@@ -2,5 +2,5 @@ export PANDA_URL_SSL=https://ai-idds-01.cern.ch:25443/server/panda
 export PANDA_URL=http://ai-idds-01.cern.ch:25080/server/panda
 export PANDA_AUTH=oidc
 export PANDA_VERIFY_HOST=off
-source /usatlas/u/podolsky/anaconda3/etc/panda/panda_setup.sh
+source /direct/eic+u/eickolja/install/etc/panda/panda_setup.sh
 export PANDA_AUTH_VO=EIC
